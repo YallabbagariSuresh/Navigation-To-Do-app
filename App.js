892d3b49dcf441071ab0b4fadac85d00c8311120
y_suresh <sureshyallabbagari@gmail.com>
2025-01-11 18:57:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css'
+import Home from './pages/Home';
+function App() {
+  return (
+    <div className="app">
+    <h1>TO-DO APP</h1>
+    <Home />
+    </div>
+  );
+}
+
+export default App;
